dfcd0ce142df95029e2d0e77a5d37fab99e44a85
nhkhiemSWE <nhkhiem.swe@gmail.com>
2025-11-07 10:51:59 -0500

    Add Sidebar component and integrate it into the RootLayout; pass user data for personalized display

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,14 @@
+import Sidebar from "@/components/Sidebar";
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const loggedInUser = {firstName: 'John', lastName: 'Doe'};
   return (
-    <main>
-      SIDEBAR
+    <main className='flex h-screen w-full font-inter'>
+      <Sidebar user={loggedInUser} />
       {children}
     </main>
   )
---
+++ components/Sidebar.tsx
@@ -0,0 +1,47 @@
+'use client'
+
+import Link from 'next/link'
+import Image from 'next/image'
+import React from 'react'
+import { sidebarLinks } from '@/constants'
+import { cn } from '@/lib/utils'
+import { usePathname } from 'next/navigation'
+
+const Sidebar = ({ user }: SiderbarProps) => {
+  const pathname = usePathname()
+  return (
+    <section className='sidebar'>
+      <nav className="flex flex-col gap-4">
+        <Link href="/"
+          className="mb-12 cursor-pointer items-center gap-2">
+            <Image 
+              src="/icons/logo.svg"
+              width={34}
+              height={34}
+              alt="logo"
+              className='size-[24px] max-xl:size-14'
+              priority
+              />
+            <h1 className='sidebar-logo'>HK Bank</h1>
+        </Link>
+        
+        {/* Sidebar Links */}
+        {sidebarLinks.map((link) => {
+          const isActive = pathname === link.route || pathname.startsWith(`${link.route}/`)
+          return (  
+            <Link key={link.label} href={link.route}
+            className={cn('sidebar-link', { 'bg-bank-gradient': isActive })}
+            >
+              <div className="relative size-6">
+                <Image src={link.imgURL} alt={link.label} fill className={cn( {'brightness-[3] invert-0' : isActive })} />
+              </div>
+              <p className= {cn('sidebar-label', { 'text-white!' : isActive })}>{link.label}</p>
+            </Link>
+          )
+        })}
+      </nav>
+    </section>
+  )
+}
+
+export default Sidebar
